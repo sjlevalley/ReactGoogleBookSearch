05fa9c4d002c935308c11a22b413ba2e1c133f3b
Steve LeValley <slevalley@gmail.com>
2021-05-24 20:08:38 -0700

    modified server page due to Heroku deployment fail

--- server.js
+++ server.js
@@ -1,54 +1,36 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const router = require("./controllers");
-// const connectDb = require("./config/connectDb");
-// const session = require("./config/session");
-const errorMiddleware = require("./util/errorMiddleware");
+const mongoose = require("mongoose");
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
-
-const mongoose = require("mongoose");
-
-function connectDb() {
-
-  (mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/google_books_db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  )
-};
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(router);
 
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google_books_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+
+
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+};
 
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-(async () => {
-  try {
-    await connectDb();
-    console.log(`🔌 MongoDB Connected`);
-
-    const app = express();
-
-    // trust proxy required for using secure cookies on Heroku
-    app.set("trust proxy", 1);
-
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-    // app.use(session),
-    app.use(router),
-      app.use(errorMiddleware);
-
-
-    app.listen(PORT, () => {
-      console.log(`🌎 ==> Server listening on port ${PORT}!`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-})();
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
